f0401f7858ae6433e47ddf9275909b70f64a59e0
Seaniesshow13 <seandavidhall@hotmail.com>
2023-10-11 09:14:20 +0100

    Test

--- Builder.java
+++ Builder.java
@@ -1,12 +1,13 @@
 public enum Builder {
 
-    FENDER, GIBSON,COLLINS;
+    FENDER, GIBSON,COLLINS,YAMAHA;
 
     public String toString(){
         switch (this) {
             case FENDER: return "Fender";
             case GIBSON: return "Gibson";
             case COLLINS: return "Collins";
+            case YAMAHA: return "Yamaha";
 
         }
         return null;
